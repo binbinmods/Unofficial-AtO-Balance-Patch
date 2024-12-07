e111bde12a39405bcaba5fc6b8f3afbb644a8dd9
binbinmods <binbinmods@gmail.com>
2024-12-07 13:59:39 -0500

    Added Epic items

--- BalancePatchCSharp/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace UnofficialBalancePatch
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.binbin.sampleplugin";
-        public const string PLUGIN_NAME = "Sample Plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
